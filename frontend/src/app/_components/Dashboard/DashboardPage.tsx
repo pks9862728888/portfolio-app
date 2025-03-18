531e184b9c71e4b3153675fafbe502c74010bf34
pks9862728888 <pks9862728888@gmail.com>
2025-03-19 00:37:38 +0530

    Added hero section

--- frontend/src/app/_components/Dashboard/DashboardPage.tsx
+++ frontend/src/app/_components/Dashboard/DashboardPage.tsx
@@ -5,14 +5,15 @@ const DashboardPage = () => {
     <div className={styles.mainDiv}>
       <div className={styles.dpWrapper}></div>
       <div className={styles.aboutSection}>
-          <p className={styles.headingLine}>Hi, I am <span className={styles.highlightName}>Pran Kumar Sarkar</span></p>
-          <p className={styles.subHeading}>
-            A passionate <span className={styles.role}>Full Stack Web Developer</span>
-          </p>
-          <p className={styles.subHeading}>
-            Crafting <strong>innovative</strong> and <strong>user-centric solutions.</strong><br />
-            <span>Let's bring your ideas to life!</span>
-          </p>
+        <p className={styles.headingLine}>Hi, I am <span className={styles.highlightName}>Pran Kumar Sarkar</span></p>
+        <p className={styles.subHeading}>
+          A passionate <span className={styles.role}>Full Stack Web Developer</span>
+        </p>
+        <p className={styles.subHeading}>
+          Crafting <strong>innovative</strong> and <strong>user-centric solutions.</strong><br/>
+          <span>Let's bring your ideas to life!</span>
+        </p>
+        {/*Social links*/}
         <div className={"flex mt-4"}>
           <a className={"mr-4"} href={"https://github.com/pks9862728888"} target={"_blank"}>
             <img src={"github.png"} alt={"github link"} className={`${styles.connectIcon} ${styles.gitIcon}`}/>
@@ -23,6 +24,14 @@ const DashboardPage = () => {
           <a href={"mailto:pks9862728888@gmail.com"} target={"_blank"}>
             <img src={"mail.png"} alt={"gmail link"} className={styles.connectIcon}/>
           </a>
+        </div>
+        <div>
+          <img src={"dice.png"} alt={"dice"} className={styles.dice}/>
+          <img src={"Sphere-Purple-Glossy.png"} alt={"sphere"} className={styles.sphere}/>
+          <img src={"hexagon3d.png"} alt={"hexagon3d"} className={styles.hexagon}/>
+          <img src={"Pill-Blue-Glossy.png"} alt={"pill"} className={styles.pill}/>
+          <img src={"RoundCube-Iridescent.png"} alt={"toroid"} className={styles.roundCube}/>
+          <img src={"SuperToroid-Black-Matte.png"} alt={"toroid"} className={styles.toroid}/>
         </div>
       </div>
     </div>
