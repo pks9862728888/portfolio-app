a477c77cf295e3b463eb8defc7d4d01b20ec2ab7
pks9862728888 <pks9862728888@gmail.com>
2025-03-18 22:37:35 +0530

    Added basic portfolio application

---
+++ src/test/java/com/demo/portfolioapp/PortfolioAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.portfolioapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
