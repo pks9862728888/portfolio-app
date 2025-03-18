7cde185a876cb5635cb0b10dc82e6e40c1cebc1d
pks9862728888 <pks9862728888@gmail.com>
2025-03-18 22:49:45 +0530

    Added frontend

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  distDir: "../src/main/resources/public"
 };
 
 export default nextConfig;
