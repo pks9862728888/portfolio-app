c5581e4fe4f011ef1e8b63b71661b6b67d5c54a3
pks9862728888 <pks9862728888@gmail.com>
2025-03-18 23:19:13 +0530

    Created sidenav

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  distDir: "../src/main/resources/public"
+  // distDir: "../src/main/resources/public"
 };
 
 export default nextConfig;
---
+++ frontend/src/app/_components/Dashboard/DashboardPage.tsx
@@ -0,0 +1,7 @@
+const DashboardPage = () => {
+  return (
+    <h1>Home</h1>
+  );
+}
+
+export default DashboardPage;
